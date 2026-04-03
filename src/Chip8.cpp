5f16a5387279028aabb38f957df4bf5786eabb3e
Eric Shi <shieric984@gmail.com>
2026-04-03 12:41:10 -0400

    finished opcodes apart from random number generator

--- src/Chip8.cpp
+++ src/Chip8.cpp
@@ -34,6 +34,7 @@ class Chip8 {
         0xF0, 0x80, 0xF0, 0x80, 0x80  // F
         };
         unsigned int gfx[64 * 32];
+        unsigned char drawFlag;
         unsigned char key[16];
         unsigned char delay_timer;
         unsigned char sound_timer;
@@ -51,7 +52,7 @@ class Chip8 {
 
 void Chip8::initialize() {
     for (int i = 0; i < 4096; i++) {
-        if (i >=0x50 and i < 0xA0) {
+        if (i >=0x50 && i < 0xA0) {
             memory[i] = font[i - 0x50];
         }
         else {
@@ -255,7 +256,7 @@ void Chip8::decode(unsigned short opcode) {
             {}; //code actual functionality later
             break;
         
-        case 0xD:
+        case 0xD: {
             unsigned int x_coord = V[X] & 63;
             unsigned int y_coord = V[Y] & 31;
             V[0xF] = 0;
@@ -279,6 +280,127 @@ void Chip8::decode(unsigned short opcode) {
                     
                 }
             }
+            drawFlag = 1;
+            break;
+        }
+
+        case 0xE:
+            switch (NN) {
+                case 0x9E:
+                    if (key[V[X]] == 1) {
+                        PC += 2;
+                    }
+                    break;
+                
+                case 0xA1:
+                    if (key[V[X]] == 0) {
+                        PC += 2;
+                    }
+                    break;
+            }
+            break;
+        
+        case 0xF:
+            switch (NN) {
+                case 0x07:
+                    V[X] = delay_timer;
+                    break;
+                
+                case 0x15:
+                    delay_timer = V[X];
+                    break;
+                
+                case 0x18:
+                    sound_timer = V[X];
+                    break;
+                
+                case 0x1E:
+                    if (I + V[X] > 0x0FFF) {
+                        V[0xF] = 1;
+                    }
+                    else {
+                        V[0xF] = 0;
+                    }
+                    I += V[X];
+                    break;
+                
+                case 0x0A:
+                    if (key[0]) {
+                        V[X] = 0;
+                    }
+                    else if (key[1]) {
+                        V[X] = 1;
+                    }
+                    else if (key[2]) {
+                        V[X] = 2;
+                    }
+                    else if (key[3]) {
+                        V[X] = 3;
+                    }
+                    else if (key[4]) {
+                        V[X] = 4;
+                    }
+                    else if (key[5]) {
+                        V[X] = 5;
+                    }
+                    else if (key[6]) {
+                        V[X] = 6;
+                    }
+                    else if (key[7]) {
+                        V[X] = 7;
+                    }
+                    else if (key[8]) {
+                        V[X] = 8;
+                    }
+                    else if (key[9]) {
+                        V[X] = 9;
+                    }
+                    else if (key[10]) {
+                        V[X] = 10;
+                    }
+                    else if (key[11]) {
+                        V[X] = 11;
+                    }
+                    else if (key[12]) {
+                        V[X] = 12;
+                    }
+                    else if (key[13]) {
+                        V[X] = 13;
+                    }
+                    else if (key[14]) {
+                        V[X] = 14;
+                    }
+                    else if (key[15]) {
+                        V[X] = 15;
+                    }
+                    else {
+                        PC -= 2;
+                    }
+                    break;
+
+                case 0x29:
+                    I = 0x50 + (5 * V[X]);
+                    break;
+                
+                case 0x33:
+                    memory[I] = V[X] / 100;
+                    memory[I + 1] = (V[X]  / 10) % 10;
+                    memory[I + 2] = V[X] % 10;
+                    break;
+                
+                case 0x55:
+                    for (int i = 0; i <= X; i++) {
+                        memory[I + i] = V[i];
+                    }
+                    break;
+                
+                case 0x65:
+                    for (int i = 0; i <= X; i++) {
+                        V[i] = memory[I + i] ;
+                    }
+                    break;
+            }
+        break;
     }
 
 }
