836a02021cae1e5e4a061e04605977e13c57fd0b
Eric Shi <shieric984@gmail.com>
2026-04-16 02:02:42 -0400

    bruh

--- tests/chip8_tests.cpp
+++ tests/chip8_tests.cpp
@@ -127,7 +127,7 @@ TEST_CASE("IBM Opcode Testing", "[opcodes]") {
         myChip8.V[1] = 5;
 
         myChip8.decode(0xD011);
-        myChip8.decode(00E0);
+        myChip8.decode(0x00E0);
         unsigned int running = 0;
         for (int i = 0; i < 2048; i++) {
             running = running | myChip8.gfx[i];
