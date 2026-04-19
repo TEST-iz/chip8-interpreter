44641b6771bfafc4fe0932a83cfa7e68620ffd49
Eric Shi <shieric984@gmail.com>
2026-04-18 22:48:01 -0400

    wrote main loop cycle

--- src/main.cpp
+++ src/main.cpp
@@ -2,16 +2,37 @@
 #include "Display.h"
 #include <SDL2/SDL.h>
 #include <iostream>
+#include <chrono>
+
+using namespace std;
+using namespace std::chrono;
 
 Display display;
 Chip8 chip8;
 
 int main(int argc, char* argv[]) {
     bool quit = false;
-    SDL_Event e;
     display.initialize();
     chip8.initialize();
+    const std::chrono::microseconds timerInterval = std::chrono::microseconds(16666);
+    const std::chrono::microseconds cpuInterval = std::chrono::microseconds(1428);
+    auto lastCPUTime = std::chrono::steady_clock::now();
+    auto lastTimerTime = std::chrono::steady_clock::now();
     while (!quit) {
+        auto currentTime = std::chrono::steady_clock::now();
         quit = display.processInput(chip8.key);
+        while (currentTime - lastCPUTime >= cpuInterval) {
+            chip8.cycle();
+            lastCPUTime += cpuInterval;
+        }
+        while (currentTime - lastTimerTime >= timerInterval) {
+            chip8.updateTimers();
+            lastTimerTime += timerInterval;
+        }
+        if (chip8.drawFlag) {
+            display.draw(chip8.gfx);
+            chip8.drawFlag = false;
+        }
     }
-}
+    return 0;
+}  
